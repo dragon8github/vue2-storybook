ae5e1c75c85552120bf86a68cc2d575dd39c28d7
dragon8github <928532756@qq.com>
2022-02-28 17:29:57 +0800

    #

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <template>
-  <div id="app">
-    <mybutton>123</mybutton>
-  </div>
+	<div id="app"></div>
 </template>
 
 
 <script>
-import mybutton from '@/components/MyButton/MyButton.vue'
 export default {
-  name: "test",
-  components: { mybutton },
+	name: 'app',
+	components: {},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
